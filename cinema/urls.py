8755238ed4ab78c33b6d029dc308f10efdc19bf6
Dede <specialized8393@gmail.com>
2024-06-07 01:49:00 +0100

    feat: create all routers

--- cinema/urls.py
+++ cinema/urls.py
@@ -1,10 +1,38 @@
 from django.urls import path
+from rest_framework.routers import DefaultRouter
 
-from cinema.views import MovieList, MovieDetail
+from cinema.views import (
+    MovieViewSet,
+    CinemaHallViewSet,
+    ActorDetailUpdateDelete,
+    ActorListCreateView,
+    GenreDetail,
+    GenreList,
+)
+
+
+router = DefaultRouter()
+router.register(r"movies", MovieViewSet, basename="movie")
 
 urlpatterns = [
-    path("movies/", MovieList.as_view(), name="movie-list"),
-    path("movies/<int:pk>/", MovieDetail.as_view(), name="movie-detail"),
-]
+    path("actors/", ActorListCreateView.as_view(), name="actor-list-create"),
+    path(
+        "actors/<int:pk>/",
+        ActorDetailUpdateDelete.as_view(),
+        name="actor-detail"
+    ),
+    path("genres/", GenreList.as_view(), name="genre-list-create"),
+    path("genres/<int:pk>/", GenreDetail.as_view(), name="genre-detail"),
+    path(
+        "cinema-halls/",
+        CinemaHallViewSet.as_view(),
+        name="cinema-hall-list-create"
+    ),
+    path(
+        "cinema-halls/<int:pk>/",
+        CinemaHallViewSet.as_view(),
+        name="cinema-hall-detail",
+    ),
+] + router.urls
 
 app_name = "cinema"
