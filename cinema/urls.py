29ca9b9a77d69fd06f8ac18f13f66403d4fd24e7
Dede <specialized8393@gmail.com>
2024-06-07 04:36:20 +0100

    fix: update routers for CinemaHall such writes in readme.md file

--- cinema/urls.py
+++ cinema/urls.py
@@ -11,15 +11,36 @@ from cinema.views import (
 )
 
 
+cinema_hall_list = CinemaHallViewSet.as_view(
+    actions={
+        "get": "list",
+        "post": "create"
+    }
+)
+cinema_hall_detail = CinemaHallViewSet.as_view(
+    actions={
+        "get": "retrieve",
+        "put": "update",
+        "patch": "partial_update",
+        "delete": "destroy",
+    }
+)
+
+
 router = DefaultRouter()
 router.register(r"movies", MovieViewSet, basename="movie")
-router.register(r"cinema_halls", CinemaHallViewSet, basename="cinema-hall")
 
 urlpatterns = [
     path("actors/", ActorList.as_view(), name="actor-list-create"),
     path("actors/<int:pk>/", ActorDetail.as_view(), name="actor-detail"),
     path("genres/", GenreList.as_view(), name="genre-list-create"),
     path("genres/<int:pk>/", GenreDetail.as_view(), name="genre-detail"),
+    path("cinema_halls/", cinema_hall_list, name="cinema-hall-list"),
+    path(
+        "cinema_halls/<int:pk>/",
+        cinema_hall_detail,
+        name="cinema-hall-detail"
+    ),
 ] + router.urls
 
 app_name = "cinema"
